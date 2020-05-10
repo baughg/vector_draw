8df4feff750d61da3cc974c77ffe89d671652ff6
Gary Baugh <baughg@tcd.ie>
2020-05-10 19:08:21 +0100

    Add template for Dimension creation

---
+++ vector_draw/Dimension.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "DrawItem.h"
+#include "Dimension2D.h"
+
+namespace drawing {
+	template<int dim>
+	class Dimension : public DrawItem
+	{
+	public:
+		Dimension() = default;
+		Dimension(const DrawInfo &param);
+		int draw(PDF &pdf) override;
+	private:
+		int width_{};
+		int dimensions_{ dim };
+	};
+
+	template<int d>
+	Dimension<d>::Dimension(const DrawInfo &param)
+		: DrawItem { param }
+	{
+	}
+
+	template<int d>
+	int Dimension<d>::draw(PDF &pdf) {
+		if constexpr(d > 2) {
+			std::vector<drawing::Dimension<d - 1>> dims;
+			const int bits_per_dimension{ parameter_.bits_per_dimension };
+			const int repeats{ 1 << bits_per_dimension };
+			dims.reserve(repeats);
+			int xstart{ parameter_.x };
+			int ystart{ parameter_.y };
+			DrawInfo draw_info{ parameter_ };
+			int one_count{};
+			const int dim_count{ d };
+			const int xmask{ static_cast<int>(dim_count % 2 != 0) };
+			const int ymask{ static_cast<int>(dim_count % 2 == 0) };
+			const int xstep{ xmask * parameter_.width };
+			const int ystep{ ymask * parameter_.width };
+
+			for (int dw{ 0 }; dw < repeats; ++dw) {
+				draw_info.x = xstart;
+				draw_info.y = ystart;
+				dims.emplace_back(drawing::Dimension<d - 1>{ drawing::DrawInfo{ draw_info } });
+				auto it{ std::end(dims) };
+				it--;
+				one_count += it->draw(pdf);
+				xstart += xstep;
+				ystart += ystep;
+			}
+
+			return one_count;
+		}
+		else {
+			Dimension2D dim2D{ parameter_ };
+			return dim2D.draw(pdf);
+		}
+	}
+}
+
--- vector_draw/vector_draw.cpp
+++ vector_draw/vector_draw.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Dimension4D.h"
 #include "pdf.h"
+#include "Dimension.h"
 
 int main()
 {
@@ -13,12 +14,15 @@ int main()
 	pdf.SetWidth(canvasSize);
 	pdf.SetHeight(canvasSize);
 	pdf.setFont(drawing::PDF::HELVETICA, 6);
-	
 	pdf.setLineWidth(1);
-	drawing::Dimension4D dim4d{ 
-		drawing::DrawInfo{50,50,50,50,dim2Dwidth,dim2Dwidth,bits_per_dimension,4} };
-	int one_count{ dim4d.draw(pdf) };
-	const int points{ 1 << (bits_per_dimension << 2) };
+
+	drawing::DrawInfo draw_info{ 50,50,50,50,dim2Dwidth,dim2Dwidth,bits_per_dimension,4 };
+	drawing::Dimension<4> dim{ draw_info };
+	dim.draw(pdf);
+	
+	//drawing::Dimension4D dim4d{ draw_info };
+	//int one_count{ dim4d.draw(pdf) };
+	//const int points{ 1 << (bits_per_dimension << 2) };
 	std::string outputFilename {"draw.pdf"};
 	std::string errMsg{};
 
