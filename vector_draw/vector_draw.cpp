26bcc3b14ff544baf28d620ae768aaf42bf0eb6d
Gary Baugh <baughg@tcd.ie>
2020-05-11 23:28:27 +0100

    generalise cost function

--- vector_draw/vector_draw.cpp
+++ vector_draw/vector_draw.cpp
@@ -7,13 +7,13 @@
 int main()
 {
 	drawing::PDF pdf;
-	const int dim2Dwidth{ 64 };
+	const int dim2Dwidth{ 32 };
 	const int bits_per_dimension{ 2 };
 		
 	pdf.setFont(drawing::PDF::HELVETICA, 6);
 	pdf.setLineWidth(1);
 	const int dim_count{ 6 };
-	const int buckets{ 3 };
+	const int buckets{ 2 };
 
 	drawing::DrawInfo draw_info{50,50,50,50,
 		dim2Dwidth,dim2Dwidth,bits_per_dimension,dim_count,buckets };
@@ -30,9 +30,6 @@ int main()
 
 	int one_count{ dim.draw(pdf) };
 	
-	//drawing::Dimension4D dim4d{ draw_info };
-	//int one_count{ dim4d.draw(pdf) };
-	//const int points{ 1 << (bits_per_dimension << 2) };
 	std::string outputFilename {"draw.pdf"};
 	std::string errMsg{};
 
